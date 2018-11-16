76c2ab25b93c82f6608f8f2cfae4396285116602
JaehunYoon <goodasd123@gmail.com>
2018-11-16 23:08:23 +0900

    Python-Flask : Data binding, gethostbyname using socket module

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-# -*- coding: utf-8 -*-
+import socket
 
-from flask import Flask, render_template
+from flask import *
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
     return "<h1>h4lo~~</h1>"
 
+
 @app.route("/hello")
 def hello():
     return render_template("hello.html")
 
+
+@app.route("/input")
+def input():
+    return render_template("input_exam.html")
+
+
+@app.route("/input/<example>")
+def input_value(example):
+    return render_template("input_exam.html", **{'example': example})
+
+
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run("127.0.0.1", 8765, debug=True)
+    # auto get hostname
+    # app.run(socket.gethostbyname(socket.gethostname()), 80, debug=False)
+    # 배포 시에는 debug를 False로 해두자
